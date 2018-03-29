eee69bc27e11392f364a4624e110a1323d2d28e1
honzamandik <j.mandik@seznam.cz>
2018-03-29 21:40:24 +0200

    k

--- src/main/java/com/github/honzamandik/adventura/uiText/HomeController.java
+++ src/main/java/com/github/honzamandik/adventura/uiText/HomeController.java
@@ -3,11 +3,12 @@ package com.github.honzamandik.adventura.uiText;
 import com.github.honzamandik.adventura.logika.IHra;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 /**
- * Kontroler, který zprostředkovává komunikaci mezi grafikou
+ * Kontroler, kterÃ½ zprostÅ™edkovÃ¡vÃ¡ komunikaci mezi grafikou
  * a logikou adventury
  * 
  * @author Filip Vencovsky
@@ -16,23 +17,30 @@ import javafx.scene.layout.GridPane;
 public class HomeController extends GridPane {
 	
 	@FXML private TextField vstupniText;
+	@FXML private TextArea vystup;
+	
+	private IHra hra;
 	
 	/**
-	 * metoda čte příkaz ze vstupního textového pole
+	 * metoda Ä�te pÅ™Ã­kaz ze vstupnÃ­ho textovÃ©ho pole
 	 * a zpracuje ho
 	 */
 	@FXML public void odesliPrikaz() {
-		System.out.println(vstupniText.getText());
+		String vystupPrikazu = hra.zpracujPrikaz(vstupniText.getText());
+		vystup.appendText("\n----------\n"+vstupniText.getText()+"\n----------\n");
+		vystup.appendText(vystupPrikazu);
 		vstupniText.setText("");
 	}
 	
 	/**
-	 * Metoda bude soužit pro předání objektu se spuštěnou hrou
-	 * kontroleru a zobrazí stav hry v grafice.
-	 * @param objekt spuštěné hry
+	 * Metoda bude souÅ¾it pro pÅ™edÃ¡nÃ­ objektu se spuÅ¡tÄ›nou hrou
+	 * kontroleru a zobrazÃ­ stav hry v grafice.
+	 * @param objekt spuÅ¡tÄ›nÃ© hry
 	 */
 	public void inicializuj(IHra hra) {
-		
+		vystup.setText(hra.vratUvitani());
+		vystup.setEditable(false);
+		this.hra = hra;
 	}
 
 }
